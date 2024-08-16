dc5d23af48cb4e7b00dd2ee85c207b078935b945
Arnaud Floriani <sarkraf.dev@gmail.com>
2024-08-16 15:45:08 +0200

    change image tag static to image_tag cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :photo
   has_many :vehicules, dependent: :destroy
   has_many :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
@@ -9,4 +10,26 @@ class User < ApplicationRecord
   validates :first_name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and whitespace" }
   validates :last_name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and whitespace" }
   validates :birthday, presence: true
+
+  after_create :generate_initials_image
+
+  def generate_initials_image
+
+    image = Cloudinary::Uploader.upload("https://via.placeholder.com/150x150.png?text=#{initials}",
+      public_id: "user_#{id}_initials",
+      transformation: [
+        { width: 150, height: 150, gravity: "center", crop: "fill" },
+        { overlay: "text:Arial_60_bold:#{initials}", gravity: "center", color: "white" }
+      ]
+    )
+
+    self.update(image_url: image['secure_url'])
+  end
+
+  private
+
+  def initials
+    "#{first_name[0].upcase}#{last_name[0].upcase}"
+  end
+
 end
